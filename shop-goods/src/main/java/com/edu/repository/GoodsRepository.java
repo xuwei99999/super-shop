0434b18222400e214a7319c9d240c060d36d4270
xw <1780996180@qq.com>
2020-01-16 21:34:23 +0800

    更新

--- shop-goods/src/main/java/com/edu/repository/GoodsRepository.java
+++ shop-goods/src/main/java/com/edu/repository/GoodsRepository.java
@@ -2,16 +2,22 @@ package com.edu.repository;
 
 import com.edu.pojo.GoodsPojo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface GoodsRepository extends JpaRepository<GoodsPojo,Integer> {
+
+    public List<GoodsPojo> findAllByGclass(int gclass);
 
     public List<GoodsPojo> findAll();
 
+<<<<<<< HEAD
     //根据gid查询
     public GoodsPojo findGoodsPojoByGid(int gid);
 
+=======
+>>>>>>> af353e616d2702817808a99a30263fc7c30d021c
 
 
 }
