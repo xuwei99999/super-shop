9394f8ad4c720c4afe8aed071beba6b3ec38bf61
H.K <1808512286@qq.com>
2020-01-16 22:01:26 +0800

    公用pojo和接口的更新

--- shop-api/src/main/java/com/edu/pojo/GoodsPojo.java
+++ shop-api/src/main/java/com/edu/pojo/GoodsPojo.java
@@ -14,11 +14,7 @@ public class GoodsPojo implements Serializable {
     private double gprice; //价格
     private int gstock; //库存
     private int gtype; //商品类型
-<<<<<<< HEAD
     private int gclass;
-=======
-    private int gclass; //1.推荐产品2.促销产品
->>>>>>> af353e616d2702817808a99a30263fc7c30d021c
     //private int pageNum; //当前页
     //private int pageSize; //显示多少条记录
     public int getGid() {
@@ -53,13 +49,7 @@ public class GoodsPojo implements Serializable {
         this.gprice = gprice;
     }
 
-    public int getGclass() {
-        return gclass;
-    }
 
-    public void setGclass(int gclass) {
-        this.gclass = gclass;
-    }
 
     public int getGtype() {
         return gtype;
--- shop-api/src/main/java/service/IGoodsService.java
+++ shop-api/src/main/java/service/IGoodsService.java
@@ -5,12 +5,9 @@ import com.edu.pojo.GoodsPojo;
 import java.util.List;
 
 public interface IGoodsService {
-<<<<<<< HEAD
     public List<GoodsPojo> queryGoodsAll();
 
     //根据gid查询
     public GoodsPojo findGoodsPojoByGid(int gid);
-=======
     public List<GoodsPojo> findAllByGclass(int gclass);
->>>>>>> af353e616d2702817808a99a30263fc7c30d021c
 }
